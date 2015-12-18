fc0e20881a15dc62b8d0eec3040ed310dcb2aa0a
Bevis <bevis@binodata.com>
2015-12-18 18:07:33 +0800

    修改swagger 設定

---
+++ SampleAPI/Controllers/SwaggerDocController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleAPI.Controllers
+{
+    /// <summary>
+    /// Swagger 範例
+    /// </summary>
+    public class SwaggerDocController : ApiController
+    {
+       
+        /// <summary>
+        /// 取得所有學生資料
+        /// </summary>
+        /// <remarks>取回所有學生資料</remarks>
+        /// <response code="500">Internal Server Error</response>
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
